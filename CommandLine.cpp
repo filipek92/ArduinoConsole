2da504b3029f7af87de4f2ae466ddaf483da0c71
Filip Richter <richter.filip@gmail.com>
2016-07-30 12:18:33 +0200

    Initialization

---
+++ CommandLine.cpp
@@ -0,0 +1,117 @@
+/*
+  Morse.cpp - Library for flashing Morse code.
+  Created by David A. Mellis, November 2, 2007.
+  Released into the public domain.
+*/
+
+#include "Arduino.h"
+#include "CommandLine.h"
+
+CommandLine::CommandLine(Stream &s): _s(s)
+{
+  setPrompt("");
+}
+
+CommandLine::CommandLine(Stream &s, const char *prompt): _s(s)
+{
+  setPrompt(prompt);
+}
+
+void CommandLine::addCommand(void (*callback)(uint8_t argc, char *argv[]), const char cmd[])
+{
+  cmd_names[cmd_cnt] = cmd;
+  cmd_ptrs[cmd_cnt] = callback;
+  cmd_lenghts[cmd_cnt] = strlen(cmd);
+  cmd_cnt++;
+}
+
+void CommandLine::doWork()
+{
+  if(_s.available()) readChar();
+}
+
+void CommandLine::setPrompt(const char *prompt){
+  _prompt = (char*)prompt;
+}
+
+void CommandLine::readChar()
+{
+  char ch;
+  ch = _s.read();
+  if(ch=='\r' || (ch=='\n' && last!='\r')){
+    buffer[buffer_ptr]=0;
+    _s.println();
+    proccessCmd();
+    buffer_ptr=0;
+    _s.print(_prompt);
+    return;
+  }
+  if(ch==127 && buffer_ptr>0){
+    _s.write(ch);
+    buffer_ptr--;
+  }
+  if(ch=='\n' || isWhiteChar(ch)){
+    return;
+  }
+  if(buffer_ptr>=BUFFER_LENGTH){
+    _s.println();
+    _s.println(F("Prilis dlouhy prikaz"));
+    _s.print(_prompt);
+    buffer_ptr=0;
+    return;
+  }
+  buffer[buffer_ptr]=ch;
+  buffer_ptr++;
+  _s.write(ch);
+  last = ch;
+}
+
+void CommandLine::proccessCmd(){
+  for(int i=0; i<cmd_cnt; i++){
+    if((strncmp(cmd_names[i], buffer, cmd_lenghts[i])==0) && ((buffer[cmd_lenghts[i]]==' ') || (buffer[cmd_lenghts[i]]==0))){
+      proccessArgs(i);
+      return;      
+    }
+  }
+  if(strcmp("help", buffer)==0){
+    _s.println("List prikazu:");
+    for(int i=0; i<cmd_cnt; i++){
+      _s.print("  ");
+      _s.println(cmd_names[i]);  
+    }  
+  }
+  _s.println("Prikaz nenalezen");
+}
+
+void CommandLine::proccessArgs(uint8_t n){
+  char *p = buffer+cmd_lenghts[n]+1;
+  uint8_t argcnt=0;
+  boolean hasArg = false;
+  if(*(p-1)==' '){
+    while(*p!=0 && argcnt<ARGS_COUNT){
+      if(hasArg){
+        if(*p==' '){
+          hasArg=false;
+          *p=0;
+        }
+      }else{
+        if(*p!=' '){
+          cmd_args[argcnt]=p;
+          hasArg=true;
+          argcnt++;
+        }
+      }
+      p++;
+    }
+  }
+  cmd_args[argcnt]=0;
+  cmd_ptrs[n](argcnt,cmd_args);
+}
+
+boolean CommandLine::isWhiteChar(char ch){
+  return (ch<32) || (ch>126);
+}
+
+Print& CommandLine::printer(){
+  return _s;
+}
---
+++ CommandLine.h
@@ -0,0 +1,46 @@
+/*
+  CommandLine_h - Library for parsing commands
+  from Serial block
+  Created by Filip Richter, April 24, 2015.
+  Released into the public domain.
+*/
+#ifndef CommandLine_h
+#define CommandLine_h
+
+#define BUFFER_LENGTH  40
+#define CMD_COUNT      20
+#define ARGS_COUNT     20
+
+#include "Arduino.h"
+#include "stdint.h"
+
+class CommandLine
+{
+  public:
+    CommandLine(Stream &s);
+    CommandLine(Stream &s, const char *prompt);
+    void addCommand(void (*fcn)(uint8_t argc, char *argv[]), const char cmd[]);
+    void doWork();
+    void setPrompt(const char *prompt);
+    Print& printer();
+  private:
+    char *_prompt;
+  
+    Stream& _s; 
+    void (*cmd_ptrs[CMD_COUNT])(uint8_t, char **);
+    const char *cmd_names[CMD_COUNT];
+    char *cmd_args[ARGS_COUNT];
+    uint8_t cmd_lenghts[CMD_COUNT];
+    uint8_t cmd_cnt=0;
+    char last;
+    
+    char buffer[BUFFER_LENGTH];
+    uint8_t buffer_ptr=0;
+    
+    void readChar();
+    void proccessCmd();
+    void proccessArgs(uint8_t n);
+    boolean isWhiteChar(char ch);
+};
+
+#endif
